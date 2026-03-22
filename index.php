a010ec72a9d4b695e7110c3b8a6fc6d5cce0f9a4
andriyzynyuk <andriyzynyuk@gmail.com>
2026-03-22 17:42:17 -0600

    Added index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Hello</title>
+    </head>
+    <body>
+        <h1>
+            <?php
+                echo "Hello";
+            ?>
+        </h1>
+    </body>
+</html>
