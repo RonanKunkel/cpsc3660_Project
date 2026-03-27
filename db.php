7476b1ef39bd9df10cfd19f0f681583c7ad44b7b
James.APH <jamzhuston@gmail.com>
2026-03-27 16:00:44 -0600

    formatted files

--- db.php
+++ db.php
@@ -1,13 +1,12 @@
 <?php
-    $host = "localhost";
-    $dbname = "jonesautodb";
-    $user = "root";
-    $pass = "";
+$host = "localhost";
+$dbname = "jonesautodb";
+$user = "root";
+$pass = "";
 
-    try {
-        $conn = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    } catch (PDOException $e) {
-        die("Connection Failed: " . $e->getMessage());
-    }
+try {
+  $conn = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+  die("Connection Failed: " . $e->getMessage());}
 ?>
