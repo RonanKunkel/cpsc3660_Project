7b7c4208368ce1134206958ae6b6ed5d81975153
James.APH <jamzhuston@gmail.com>
2026-03-28 02:24:34 -0600

    cleaned up code

--- templates/header.php
+++ templates/header.php
@@ -6,6 +6,8 @@
   <nav>
     <button onclick="window.location.href='purchaseCar.php'">Purchase Vehicle</button>
     <button onclick="window.location.href='carSales.php'">Sell A Car</button>
-    <button onclick="window.location.href='customer.php'">Add a customer</button>
+    <button onclick="window.location.href='customer.php'">Add a Customer</button>
+    <button onclick="window.location.href='index.php'">Home</button>
+    <button onclick="window.location.href='payments.php'">Add Customer Payments</button>
   </nav>
 </header>
