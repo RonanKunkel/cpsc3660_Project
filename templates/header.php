7fbe28ba8e73fd8b8b23200931a464cca60aa977
James.APH <jamzhuston@gmail.com>
2026-03-27 19:36:15 -0600

    added a template for navigation

---
+++ templates/header.php
@@ -0,0 +1,11 @@
+<head>
+  <title>Jones Auto</title>
+</head>
+ 
+<header> 
+  <nav>
+    <button onclick="window.location.href='purchaseCar.php'">Purchase Vehicle</button>
+    <button onclick="window.location.href='carSales.php'">Sell A Car</button>
+    <button onclick="window.location.href='customer.php'">Add a customer</button>
+  </nav>
+</header>
